6d9155b1b1f4bb6c3ea0965dade64d78651de144
Eran Leiser <eran.l@cheq.ai>
2022-10-08 17:03:20 +0300

    Change Avatar picture + change pic

--- src/components/partials/HeroSection.tsx
+++ src/components/partials/HeroSection.tsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
       <HeroBackground />
       <div className="flex flex-col items-center">
         <div className="overflow-hidden rounded-full">
-          <Image src="/images/avatar/man.png" width={180} height={180} alt="avatar" />
+          <Image src="/images/avatar/Eran_1.png" width={180} height={180} alt="avatar" />
         </div>
         <h1 className="mt-4 text-3xl font-bold ">Eran Leiser</h1>
         <div className="mt-2 flex gap-1">

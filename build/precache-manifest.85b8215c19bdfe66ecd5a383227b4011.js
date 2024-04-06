df75a128abd691ca509f3a5302e2cd7f45d4dfe0
Eran Leiser <eran1205@gmail.com>
2024-04-06 15:49:20 +0300

    Add main branch

---
+++ build/precache-manifest.85b8215c19bdfe66ecd5a383227b4011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f59f98fb161bf171e85",
+    "url": "/static/css/main.8dd6d15e.chunk.css"
+  },
+  {
+    "revision": "1f59f98fb161bf171e85",
+    "url": "/static/js/main.1f59f98f.chunk.js"
+  },
+  {
+    "revision": "f7863ee15824e544f112",
+    "url": "/static/js/1.f7863ee1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "78dc5d9c3359335edeb9561780949731",
+    "url": "/index.html"
+  }
+];

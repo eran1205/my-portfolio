f478269656b5130b9ac91e2779d948ad352d59fb
Eran Leiser <eran1205@gmail.com>
2024-04-06 15:21:00 +0300

    Change work details

--- src/resumeData.js
+++ src/resumeData.js
@@ -27,62 +27,69 @@ let resumeData = {
     "website":"https://rbhatia46.github.io",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Afeka Academic College of Engineering in Tel-Aviv",
+        "specialization":"",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2006",
+        "Achievements":"Bachelor's degree (B.Sc) in software engineering"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Campus Engineering and Technology Studies",
+        "specialization":"",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2000",
+        "Achievements":"Engineer's degree certificate on behalf of Mahat"
       }
     ],
     "work":[
       {
         "CompanyName":"CHEQ.AI",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "specialization":"Technologies: React, NodeJS, Golang, Typescript, Docker, Microservices, full CI/CD, K8S, AWS. Databases: MySQL, Postgresql, DynamoDB and Clickhouse",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":"Manage 7 fullstack engineers, Responsible for live application with over 600 enterprise clients. The application includes couple of dashboards, reports and analytics mechanism."
       },
       {
         "CompanyName":"AT&T",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "specialization":"Technologies: React, Angular, NodeJS, Java. Databases: Oracle, MongoDB and Couchbase",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2022",
+        "Achievements":"Manage some projects like: My AT&T, Transmission of commercials for video clients of AT&T and internal finance system etc."
       },
       {
         "CompanyName":"BIO-RAD",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "specialization":"Technologies: VB.NET, Angular",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"2014",
+        "Achievements":"BriCare system"
       },
       {
         "CompanyName":"RAMDOR",
-        "specialization":"Some specialization",
+        "specialization":"Technologies: React, NodeJS and Javascript",
         "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "YearOfLeaving":"2012",
+        "Achievements":"Develop new SASS system: Ramdor.net"
       },
       {
         "CompanyName":"PAYONEER",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "specialization":"Technologies: Angular, NodeJS and Javascript",
+        "MonthOfLeaving":"Sep",
+        "YearOfLeaving":"2009",
+        "Achievements":""
       },
       {
         "CompanyName":"ONE SPORTS",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "specialization":"Technologies: C#",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2007",
+        "Achievements":"Working on CrazyOne: Application that allowed users to upload videos and rate them."
+      },
+      {
+        "CompanyName":"IAI",
+        "specialization":"Technologies: VB.NET",
+        "MonthOfLeaving":"Jul",
+        "YearOfLeaving":"2006",
+        "Achievements":""
       }
     ],
     "skillsDescription":"Your skills here",
@@ -103,7 +110,7 @@ let resumeData = {
         "skillname":"CSS"
       },
       {
-        "skillname":"Node.js"
+        "skillname":"Nodejs"
       },
       {
         "skillname":"GoLang"
@@ -136,12 +143,12 @@ let resumeData = {
     ],
     "testimonials":[
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"It was a great pleasure working with Eran. Eran is one of a kind. Always like to help and assist others with his deep knowledge. He consistently gave 100% of himself to the group and ensured to deliver the content on time. Eran is very reliable and professional, and at stressful times he was a positive influence on all of us. If you’re looking for an exceptional manager who can help your team achieve their full potential, look no further.",
+        "name":"Aviad Avisar (Team Leader at AT&T)"
       },
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"Eran was my co-worker and he is one of the best Developer, manager and colleague I have been working with. Always on track and contributes all of his energy to execute the excellent products. Hopefully we will be working together again. ",
+        "name":"Noa Buchman (Product owner at CHEQ.AI)"
       }
     ]
   }
